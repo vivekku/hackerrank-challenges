cb58191d7fb4dd83ed0ba3120f3a51e47048b6c7
Vivek Kumar <vivek.it.1108@gmail.com>
2018-12-27 10:35:46 +0530

    Added Javascript source file named Simple_Array_Sum.js for Hackerrank Challenge named Simple Array Sum

---
+++ Day_1/Simple_Array_Sum.js
@@ -0,0 +1,10 @@
+function simpleArraySum(ar) {
+    /*
+     * Write your code here.
+     */
+    var sum = 0;
+    for (var i = 0; i < ar.length; i++){
+        sum += ar[i];
+    }
+    return sum;
+}
