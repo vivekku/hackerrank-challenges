3c8e6a2b8b2282619f094a340dbfd47276d25e85
Vivek Kumar <vivek.it.1108@gmail.com>
2018-12-27 11:01:05 +0530

    Added Javascript source file named Save_the_Prisoner.js for Hackerrank Challenge named Save the Prisoner

---
+++ Day_2/Save_the_Prisoner.js
@@ -0,0 +1,12 @@
+function saveThePrisoner(n, m, s) {
+    if (s + m - 1 < n) {
+
+        return s + m - 1;
+    } else {
+        if ((s + m - 1) % n == 0) {
+            return n;
+        } else {
+            return (s + m - 1) % n;
+        }
+    }
+}
