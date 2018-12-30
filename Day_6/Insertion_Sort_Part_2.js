c50a37307a863afe135094a2d386e1e2e0683365
Vivek Kumar <vivek.it.1108@gmail.com>
2018-12-31 02:20:27 +0530

    Added Javascript Source File named Insertion_Sort_Part_2.js for Hackerrank Challenge named Insertion Sort Part 2

---
+++ Day_6/Insertion_Sort_Part_2.js
@@ -0,0 +1,12 @@
+function insertionSort2(n, arr) {
+    for (let i = 1; i < n; i++) { 
+        let key = arr[i]; 
+        let j = i-1; 
+        while (j >= 0 && arr[j] > key) { 
+            arr[j+1] = arr[j]; 
+            j = j-1; 
+        } 
+        arr[j+1] = key;
+        console.log(...arr);
+    }
+}
