3a43f6e1c1a33afbcea056b15de1b40d6d96eb70
Vivek Kumar <vivek.it.1108@gmail.com>
2018-12-31 01:48:33 +0530

    Added Javascript Source File named Find_Strings.js for Hackerrank Challenge named Find Strings

---
+++ Day_5/Find_Strings.js
@@ -0,0 +1,31 @@
+function findStrings(w, queries) {
+    let suffixes = new Set();
+    
+    for (let str of w) {
+        for (let i = 0; i < str.length; i++) {
+            suffixes.add(str.substr(i, str.length - i));
+        }
+    }
+    
+    let sortedSuffixes = Array.from(suffixes).sort((a, b) => a > b ? 1 : -1);
+    let lcps = [0];
+    
+    for (let i = 1; i < sortedSuffixes.length; i++) {
+        lcps.push(lcp(sortedSuffixes[i], sortedSuffixes[i - 1]));
+    }
+    
+    return queries.map(q => {
+        for (let i = 0; i < sortedSuffixes.length; i++) {
+            let suffix = sortedSuffixes[i];
+            let lcp = lcps[i];
+            
+            if (q > suffix.length - lcp) {
+                q -= suffix.length - lcp;
+            } else {
+                return suffix.substr(0, q + lcp);
+            }
+        }
+        
+        return 'INVALID';
+    });
+}
