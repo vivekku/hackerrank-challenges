772d95326e13a387ac0a0928f24190b5604bd15b
Vivek Kumar <vivek.it.1108@gmail.com>
2018-12-31 14:09:12 +0530

    Added Java Source File named Binary_Search_Tree_Lowest_Common_Ancestor.java for Hackerrank Challenge named Binary Search Tree Lowest Common Ancestor

--- Day_9/Binary_Search_Tree_Lowest_Common_Ancestor.java
+++
@@ -1,9 +0,0 @@
-public static Node lca(Node root, int v1, int v2) {
-      	if(v1 > root.data && v2 > root.data){
-              return lca(root.right, v1, v2);
-          } else if(v1 < root.data && v2 < root.data) {
-            return lca(root.left, v1, v2);
-        } else {
-            return root;
-        }
-    }
