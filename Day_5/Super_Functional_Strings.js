6dbff70380c725774a21a11c2933737c3c595bbd
Vivek Kumar <vivek.it.1108@gmail.com>
2018-12-31 16:34:22 +0530

    Added Javascript source file named Super_Functional_Strings.js for Hackerrank challenge named Super Functional Strings

--- Day_5/Super_Functional_Strings.js
+++ Day_5/Super_Functional_Strings.js
@@ -1,30 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(str => str.trim());
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the superFunctionalStrings function below.
- */
 function superFunctionalStrings(s) {
     
     var i,j;
@@ -68,19 +41,4 @@ function findPow(a,b){
         result = (result *a)%(Math.pow(10,9)+7)
     }
     return result;
-}
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const t = parseInt(readLine(), 10);
-
-    for (let tItr = 0; tItr < t; tItr++) {
-        const s = readLine();
-
-        let result = superFunctionalStrings(s);
-
-        ws.write(result + "\n");
-    }
-
-    ws.end();
 }
