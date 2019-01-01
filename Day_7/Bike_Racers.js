9eb7d57d73f50bdc9b722c6fced4a4ff826a2b71
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-02 00:39:28 +0530

    Added JavaScript source file named Bike_Racers.js for HackerRank Challenge named Bike Racers

---
+++ Day_7/Bike_Racers.js
@@ -0,0 +1,76 @@
+function bikeRacers(bikers, bikes, K) {
+
+    function maxBipartiteMatch(matrix, isConnected) {
+        function hasMatch(u) {
+            for (var v = 0; v < M; v++) {
+                if (isConnected(matrix[u][v]) && !visited[v]) {
+                    visited[v] = 1;
+
+                    if (matches[v] === -1 || hasMatch(matches[v])) {
+                        matches[v] = u;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        var N = matrix.length;
+        var M = matrix[0].length;
+        var visited = new Array(M);
+        var matches = new Array(M);
+        var res = 0;
+        isConnected = isConnected || function (x) { return x };
+        for (var i = 0; i < M; i++) {
+            matches[i] = -1;
+        }
+        for (var u = 0; u < N; u++) {
+            for (var k = 0; k < M; k++) {
+                visited[k] = 0;
+            }
+            if (hasMatch(u)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    var origMatrix = [];
+    var workMatrix = [];
+    var sortArr = [];
+
+    for (var i = 0; i < bikers.length; i++) {
+        origMatrix[i] = [];
+        workMatrix[i] = [];
+        for (var j = 0; j < bikes.length; j++) {
+        var v = Math.pow(Math.abs(bikers[i][0] -  bikes[j][0]), 2) + Math.pow(Math.abs(bikers[i][1] - bikes[j][1]), 2)
+        origMatrix[i][j] = v;
+        workMatrix[i][j] = v;
+        sortArr.push(v);
+        }
+    }
+    sortArr.sort((a,b) => a - b);
+    var left = 0;
+    var mid = 0;
+    var right = sortArr.length - 1;
+    var ans = sortArr[right];
+    while (left <= right) {
+        mid = left + Math.ceil((right - left) / 2);
+        for (var i = 0; i < bikers.length; i++) {
+            for (var j = 0; j < bikes.length; j++) {
+                if (origMatrix[i][j] <= sortArr[mid]) { 
+                    workMatrix[i][j] = 0;
+                } else {
+                    workMatrix[i][j] = origMatrix[i][j];
+                } 
+            }
+        }
+        var matchN = maxBipartiteMatch(workMatrix, (x) => !x);
+        if (matchN < K) {
+        left = mid + 1;
+        } else {
+            right = mid - 1;
+            ans = Math.min(ans, sortArr[mid]);
+        }
+    }
+    return ans;
+}
