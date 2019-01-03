b66422135b0737e4478fe0b451d8ae2c1f9672ae
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-03 23:00:31 +0530

    Added JavaScript source file named Array_Pairs.js for HackerRank challenge named Array Pairs

--- Day_9/Array_Pairs.js
+++ Day_9/Array_Pairs.js
@@ -1,10 +1,9 @@
 function solve(arr) {
-    var max = Math.max(...arr);
-    var count = 0, product;
-    for (var i = 0; i < arr.length - 1; i++){
-        for (var j = i + 1; j < arr.length; j++){
-            product = arr[i] * arr[j];
-            if (product <= max) {
+    var count = 0, pairs = [];
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            var max = Math.max(...arr.slice(i, j + 1));
+            if (arr[i] * arr[j] <= max) {
                 count++;
             }
         }
