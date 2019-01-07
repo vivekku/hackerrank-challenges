4574c76550d519a78cb5196ce382697c6928ad3c
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-07 16:17:42 +0530

    Added JavaScript source file named Taxicab_Drivers_Problem.js for HackerRank challenge named Taxicab Drivers Problem

--- Day_13/Taxicab_Drivers_Problem.js
+++ Day_13/Taxicab_Drivers_Problem.js
@@ -1,49 +1,61 @@
-function processData(input) {
-    var input_stdin_array = '';
-    var array = [];
-    input_stdin_array = input.split('\n');
-    var n = parseInt(input_stdin_array[0]);
-    var line = input_stdin_array[1].split(' ');
-    for (var i=0; i<n; i++) {
-        array[i+1] = parseInt(line[i]);
-    }
-    var max = 0;
-    if (n == 1 || n == 2) {
-        console.log(0);
-    } else {
-        for (var i=2; i<n; i++) {
-            var left = leftIndex(i);
-            var right;
-            if (left != 0) {
-                right = rightIndex(i);
-                if (right != 0) {
-                    var index_product = left * right;
-                    if (index_product > max) {
-                        max = index_product;
-                    }
-                }
-            }
-        }
-        console.log(max);   
-    }
+function Graph() { 
+    var node=[], adj=[]; node.push(null); adj.push(null);
+    return { 
+        addN: function(x,y) {node.push({x:x,y:y});}, 
+        addE: function(v,w) { 
+            if(adj[v]==null) adj[v] = [];
+            if(adj[w]==null) adj[w] = [];
+            adj[v].push(w);
+            adj[w].push(v);
+        },
+        adj: function(v) { return adj[v]; },
+        dist: function(v,w) { 
+            var x=Math.abs(node[v].x-node[w].x);
+            var y=Math.abs(node[v].y-node[w].y);
+            return {x:x,y:y};
+        },
+        n:node, 
+        a:adj };
+}
 
-    function leftIndex(index) {
-        for (var j=index-1; j>=1; j--) {
-            if (array[j] > array[index]) {
-                return j;
+function BFS(G,s,N,H,V) { 
+    var q=[], m=[], reached=0, i;
+    q.push(s); m[s]={x:0,y:0};
+    while(q.length>0) {
+        var v=q.shift(), adj=G.adj(v), n=adj.length;
+        for (i=0;i<n;++i) {
+            var w=adj[i], d=G.dist(v,w), dH=d.x+m[v].x, dV=d.y+m[v].y;
+            if ( dH>H || dV>V ) continue;
+            if (m[w]==null || m[w].x>dH || (m[w].x==dH && m[w].y>dV)) {
+                m[w]={x:dH,y:dV};
+                q.push(w);
             }
         }
-        return 0;
     }
-    
-    function rightIndex(index) {
-        for (var k=index+1; k<=n; k++) {
-            if (array[k] > array[index]) {
-                return k;
-            }
-        }
-        return 0;
+    for ( i=1,reached=0; i<=N; ++i ) { if ( m[i]!=null) ++reached; }
+    return { 
+        m:m, reached:reached, notReached:N-reached
+    };
+}
+
+function processData(input) {
+    input = input.split(/\s+/); var index=0;
+    function get() { return input[index++]|0; }
+    var N=get(), H=get(), V=get(), i, G = new Graph();
+    for ( i=0; i<N; ++i ) {
+        var x=get(), y=get();
+        G.addN(x,y);
     }
+    for ( i=0; i<N-1; ++i ) {
+        var v=get(), w=get();
+        G.addE(v,w);
+    }
+    var cnt=0;
+    for ( i=1; i<=N; ++i ) {
+        var b = BFS(G,i,N,H,V);
+        cnt += b.notReached;
+    }
+    console.log(cnt/2);
 } 
 
 process.stdin.resume();
