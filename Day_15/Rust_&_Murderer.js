ab99590821238b92d562ce73f55c31ac0564508b
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-08 17:41:05 +0530

    Added JavaScript source file named Rust_&_Murderer.js for HackerRank challenge named Rust & Murderer

--- Day_15/Rust_&_Murderer.js
+++ Day_15/Rust_&_Murderer.js
@@ -1,3 +1,27 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
 function rustMurderer(n, roads, s) {
     let roadsHash = {};
     roads.forEach(road => {
@@ -29,4 +53,32 @@ function rustMurderer(n, roads, s) {
     }
     nodesDistance.splice(s, 1);
     return nodesDistance;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const nm = readLine().split(' ');
+
+        const n = parseInt(nm[0], 10);
+
+        const m = parseInt(nm[1], 10);
+
+        let roads = Array(m);
+
+        for (let roadsRowItr = 0; roadsRowItr < m; roadsRowItr++) {
+            roads[roadsRowItr] = readLine().split(' ').map(roadsTemp => parseInt(roadsTemp, 10) - 1);
+        }
+
+        const s = parseInt(readLine(), 10) - 1;
+
+        let result = rustMurderer(n, roads, s);
+
+        ws.write(result.join(" ") + "\n");
+    }
+
+    ws.end();
 }
