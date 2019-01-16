0ae01b0b49395c2ff9db1f10387ad1fed1010642
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-16 18:21:20 +0530

    Added Java source file named PatternSyntaxChecker.java for HackerRank challenge named Pattern Syntax Checker

---
+++ Day_20/PatternSyntaxChecker.java
@@ -0,0 +1,18 @@
+public class PatternSyntaxChecker {
+    public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+            try {
+               Pattern.compile(pattern);
+               System.out.println("Valid");
+           } catch (PatternSyntaxException exception) {
+               System.out.println("Invalid");
+           }
+		}
+	}
+}
+
+
+
