f9fd43052fbb0d5face43bcedecfae9341480299
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-17 16:42:46 +0530

    Added Java source file named ValidUsernameRegularExpression.java for HackerRank challenge named Valid Username Regular Expression

---
+++ Day_21/ValidUsernameRegularExpression.java
@@ -0,0 +1,20 @@
+class UsernameValidator {
+    public static final String regularExpression = "^([A-Z]|[a-z])\\w{7,29}$";
+}
+
+public class ValidUsernameRegularExpression {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
