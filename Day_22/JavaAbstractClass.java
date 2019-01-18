4a5d0a3dff26534e204812ae514e1cf4c546312b
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-18 10:48:40 +0530

    Added Java source file named JavaAbstractClass.java for HackerRank challenge named Java Abstract Class

---
+++ Day_22/JavaAbstractClass.java
@@ -0,0 +1,16 @@
+class MyBook extends Book {
+    void setTitle(String s) {
+        this.title = s;
+    }
+}
+
+public class JavaAbstractClass{	
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();		
+	}
+}
