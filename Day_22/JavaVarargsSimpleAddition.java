9dec6cf90fa3ac8524ed77add5b2ce6fb3b6d8a7
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-18 10:56:58 +0530

    Added Java source file named JavaVarargsSimpleAddition.java for HackerRank challenge named Java Varargs Simple Addition

---
+++ Day_22/JavaVarargsSimpleAddition.java
@@ -0,0 +1,45 @@
+class Add {    
+    void add(int... numbers) {
+        String sumString= "";
+        int sumInt = 0;
+        for (int nr : numbers) {
+            sumInt += nr;
+            sumString += nr + "+";
+        }
+        System.out.println(sumString.substring(0, sumString.length()-1) + "=" + sumInt);
+    }
+}
+
+public class JavaVarargsSimpleAddition {
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++) {
+				if(set.contains(methods[i].getName())) {
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());				
+			}
+			if(overload) {
+				throw new Exception("Overloading not allowed");
+			}
+		} catch(Exception e) {
+				e.printStackTrace();
+		}
+	}
+}
