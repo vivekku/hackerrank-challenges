5b9e4f1e9bea535c4ad94c41419a355c6c4b5699
Vivek Kumar <vivek.it.1108@gmail.com>
2019-01-18 10:57:43 +0530

    Added Java source file named PrimeChecker.java for HackerRank challenge named Prime Checker

---
+++ Day_22/PrimeChecker.java
@@ -0,0 +1,60 @@
+import static java.lang.System.in;
+class Prime{
+    public void checkPrime(int... n){
+		boolean f = false;
+		for(int x :n){
+			if(isPrime(x)){
+				if (f)
+					java.lang.System.out.print(" ");
+				f = true;
+				java.lang.System.out.print(x);
+			}
+		}
+		System.out.println();
+    }
+    private boolean isPrime(int x){
+        if(x<2) return false;
+        for(int i=2;i*i<=x;i++){
+            if(x%i==0) return false;
+        }
+        return true;
+    }
+}
+
+public class PrimeChecker {
+	public static void main(String[] args) {
+		try{
+		BufferedReader br=new BufferedReader(new InputStreamReader(in));
+		int n1=Integer.parseInt(br.readLine());
+		int n2=Integer.parseInt(br.readLine());
+		int n3=Integer.parseInt(br.readLine());
+		int n4=Integer.parseInt(br.readLine());
+		int n5=Integer.parseInt(br.readLine());
+		Prime ob=new Prime();
+		ob.checkPrime(n1);
+		ob.checkPrime(n1,n2);
+		ob.checkPrime(n1,n2,n3);
+		ob.checkPrime(n1,n2,n3,n4,n5);	
+		Method[] methods=Prime.class.getDeclaredMethods();
+		Set<String> set=new HashSet<>();
+		boolean overload=false;
+		for(int i=0;i<methods.length;i++)
+		{
+			if(set.contains(methods[i].getName()))
+			{
+				overload=true;
+				break;
+			}
+			set.add(methods[i].getName());			
+		}
+		if(overload)
+		{
+			throw new Exception("Overloading not allowed");
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}	
+}
